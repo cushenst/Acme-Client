32a9cb24b5f763fa70477887e56f1e10e5a2307d
Stephen Cushen <6130247+cushenst@users.noreply.github.com>
2021-10-15 12:13:39 +0200

    use pebble ca

--- student_source/make_requests.py
+++ student_source/make_requests.py
@@ -3,22 +3,22 @@ import requests
 
 def get_urls(url):
     print(f"GET : {url}")
-    return requests.get(url, verify=False)
+    return requests.get(url)
 
 
 def post_data(url, data):
     print(f"POST to: {url}")
     create_account_request = requests.post(url, headers={"Content-Type": "application/jose+json"},
-                                           data=data, verify=False)
+                                           data=data)
 
     return create_account_request.headers, create_account_request.content.decode()
 
 
 def get_nonce(url):
     print("GET Nonce")
-    return requests.head(url, verify=False)
+    return requests.head(url)
 
 
 def get_http(url):
     print(f"GET {url}")
-    return requests.get(url, verify=False)
+    return requests.get(url)
